143a46d3bd81a9962ae6d6ccbb3d05f70828f3ea
Edith Balbouze <edithbalbouze@Ediths-MacBook-Air-2.local>
2023-07-24 16:55:56 -0400

    Curso de react [2023]: De cero hasta crear tus primeros componentes con estado v3

---
+++ projects/00-hola-mundo/src/assets/App.jsx
@@ -0,0 +1,6 @@
+import './App.css';
+import {TwitterFollowCard} from "./TwitterFollowCard.jsx";
+export function App () {
+    return (
+        <section className="App">
+            <TwitterFollowCard isFollowing userName="fjmn2001">
